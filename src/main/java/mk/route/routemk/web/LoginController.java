d5ee3fca5a8f1d17c1b16d986ec267d74ec1d528
David Dukoski <duko_7@outlook.com>
2025-01-29 19:34:45 +0100

    feat: Add logout option in state authenticated/logged in.

--- src/main/java/mk/route/routemk/web/LoginController.java
+++ src/main/java/mk/route/routemk/web/LoginController.java
@@ -1,5 +1,7 @@
 package mk.route.routemk.web;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,11 @@ public class LoginController {
     @GetMapping
     public String loginPage(Model model) {
         model.addAttribute("display", "login");
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        model.addAttribute("principal", auth.getPrincipal().toString());
+
         return "master";
     }
 
--- src/main/java/mk/route/routemk/web/RegisterController.java
+++ src/main/java/mk/route/routemk/web/RegisterController.java
@@ -1,6 +1,8 @@
 package mk.route.routemk.web;
 
 import mk.route.routemk.services.interfaces.AccountService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,13 +35,20 @@ public class RegisterController {
                                 @RequestParam String surname,
                                 @RequestParam String email,
                                 @RequestParam String password,
-                                @RequestParam String confirmPassword) {
+                                @RequestParam String confirmPassword,
+                                Model model) {
 
         try {
             accountService.save(email, name, surname, password, confirmPassword);
         } catch (Exception e) {
             return String.format("redirect:/register?error=%s", e.getMessage());
         }
+
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        model.addAttribute("principal", auth.getPrincipal().toString());
+
 
         return "redirect:/login";
     }
