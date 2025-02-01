aa6c4621db7c11fcfe5af513737d0ba6a39388fe
KikoTheFinker <kristijanstojanovskii24@gmail.com>
2025-02-01 17:48:43 +0100

    refactor: Change in Trip model, CompanyTripController is less burdened and small changes in thymeleaf.

--- src/main/java/mk/route/routemk/models/Trip.java
+++ src/main/java/mk/route/routemk/models/Trip.java
@@ -3,13 +3,13 @@ package mk.route.routemk.models;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import mk.route.routemk.models.enums.Status;
 
 import java.time.LocalDate;
 import java.util.Collection;
 
 @Data
 @Entity
-@NoArgsConstructor
 @Table(name = "trip")
 public class Trip {
 
@@ -38,18 +38,53 @@ public class Trip {
             joinColumns = @JoinColumn(name = "trip_id"),
             inverseJoinColumns = @JoinColumn(name = "location_id")
     )
-    private Collection<Location> stops;
+    private Collection<TripStop> stops;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    public int getTripId() {
+        return tripId;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public TransportOrganizer getTranOrg() {
+        return tranOrg;
+    }
+
+    public Route getRoute() {
+        return route;
+    }
+
+    public int getFreeSeats() {
+        return freeSeats;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public Collection<TripStop> getStops() {
+        return stops;
+    }
 
     public Trip(TransportOrganizer tranOrg,
-                Collection<Location> stops,
+                Collection<TripStop> stops,
                 Route route,
                 int freeSeats,
-                LocalDate date) {
+                LocalDate date,
+                Status status) {
         this.tranOrg = tranOrg;
         this.route = route;
         this.freeSeats = freeSeats;
         this.date = date;
         this.stops = stops;
+        this.status = status;
     }
-
+    public Trip() {
+    }
 }
--- src/main/java/mk/route/routemk/web/company/CompanyTripController.java
+++ src/main/java/mk/route/routemk/web/company/CompanyTripController.java
@@ -1,12 +1,9 @@
 package mk.route.routemk.web.company;
 
-import mk.route.routemk.models.Location;
 import mk.route.routemk.models.Trip;
 import mk.route.routemk.services.company.CompanyTripService;
 import mk.route.routemk.services.interfaces.LocationService;
 import mk.route.routemk.services.interfaces.RouteService;
-import mk.route.routemk.services.interfaces.TripStopService;
-import mk.route.routemk.specifications.TripStopSpecification;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,23 +18,19 @@ public class CompanyTripController {
     private final CompanyTripService companyTripService;
     private final LocationService locationService;
     private final RouteService routeService;
-    private final TripStopService tripStopService;
 
-    public CompanyTripController(CompanyTripService companyTripService, LocationService locationService, RouteService routeService, TripStopService tripStopService) {
+    public CompanyTripController(CompanyTripService companyTripService, LocationService locationService, RouteService routeService) {
         this.companyTripService = companyTripService;
         this.locationService = locationService;
         this.routeService = routeService;
-        this.tripStopService = tripStopService;
     }
 
     @GetMapping("/{routeId}")
     public String routeTrips(@PathVariable Integer routeId, Model model) {
         List<Trip> trips = companyTripService.getAuthorizedTripsByRoute(routeId);
-        List<Integer> tripIds = trips.isEmpty() ? List.of() : trips.stream().map(Trip::getTripId).toList();
 
         model.addAttribute("trips", trips);
         model.addAttribute("locations", locationService.findAll());
-        model.addAttribute("tripStops", tripStopService.findAllByPredicate(TripStopSpecification.tripStopsByTripIds(tripIds)));
         model.addAttribute("routeSource", routeService.findById(routeId).getSource());
         model.addAttribute("routeDestination", routeService.findById(routeId).getDestination());
         model.addAttribute("display", "/company/company-view-trip");
