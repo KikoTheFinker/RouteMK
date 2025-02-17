5338ff1a0799f90aef45071091fe9580a7e7c601
KikoTheFinker <kristijanstojanovskii24@gmail.com>
2025-02-17 11:25:31 +0100

    refactor: Change POST & GET mapping to only GET mapping for search-routes. Add notification if no routes are found.

--- src/main/java/mk/route/routemk/web/user/UserRouteController.java
+++ src/main/java/mk/route/routemk/web/user/UserRouteController.java
@@ -2,11 +2,9 @@ package mk.route.routemk.web.user;
 
 import mk.route.routemk.models.Route;
 import mk.route.routemk.services.interfaces.RouteService;
-import mk.route.routemk.services.interfaces.TripService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -17,24 +15,24 @@ import java.util.List;
 public class UserRouteController {
     private final RouteService routeService;
 
-    public UserRouteController(RouteService routeService, TripService tripService) {
+    public UserRouteController(RouteService routeService) {
         this.routeService = routeService;
     }
 
     @GetMapping
-    public String display(Model model) {
-        model.addAttribute("routes", routeService.findAll());
-        model.addAttribute("display", "user/search-routes");
-        return "master";
-    }
-
-    @PostMapping
     public String findRoutesByFromAndTo(@RequestParam(required = false) String from,
                                         @RequestParam(required = false) String to,
                                         Model model) {
-        List<Route> filteredRoutes = routeService.findRouteByFromAndToDest(from, to);
-        model.addAttribute(filteredRoutes.isEmpty() ? "noRoutesMessage" : "routes", filteredRoutes.isEmpty() ? "No routes found for your search." : filteredRoutes);
+
+        List<Route> filteredRoutes = ((from == null || from.isBlank()) && (to == null || to.isBlank()))
+                ? routeService.findAll()
+                : routeService.findRouteByFromAndToDest(from, to);
+
+        model.addAttribute(filteredRoutes.isEmpty() ? "noRoutesMessage" : "routes",
+                filteredRoutes.isEmpty() ? "No routes found for your search." : filteredRoutes);
         model.addAttribute("display", "user/search-routes");
+
         return "master";
     }
+
 }
