63edfa8f41da98dfe1e9baf7a5452acff92964d5
Jana-Angjelkoska-v8 <jana.angjelkoska@vector8.com>
2025-08-30 15:43:18 +0200

    fix: Details page for trip enchanced

--- src/main/java/mk/route/routemk/services/ReviewServiceImpl.java
+++ src/main/java/mk/route/routemk/services/ReviewServiceImpl.java
@@ -5,6 +5,7 @@ import mk.route.routemk.repostories.interfaces.GenericRepository;
 import mk.route.routemk.repostories.interfaces.ReviewRepository;
 import mk.route.routemk.services.interfaces.ReviewService;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 import static mk.route.routemk.specifications.ReviewSpecification.*;
@@ -12,15 +13,12 @@ import static mk.route.routemk.specifications.ReviewSpecification.*;
 @Service
 public class ReviewServiceImpl extends GenericServiceImpl<Review, Integer> implements ReviewService {
 
-    private final ReviewRepository reviewRepository;
-    public ReviewServiceImpl(GenericRepository<Review, Integer> genericRepository, ReviewRepository reviewRepository) {
+    public ReviewServiceImpl(GenericRepository<Review, Integer> genericRepository) {
         super(genericRepository);
-        this.reviewRepository = reviewRepository;
     }
 
     @Override
     public List<Review> findReviewsForTrip(Integer tripId) {
-        System.out.println(reviewRepository.findAll());
         return findAllByPredicate(hasTripId(tripId)).stream().toList();
     }
 
