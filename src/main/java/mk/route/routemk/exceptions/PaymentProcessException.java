b321a59fcc22c14e551182532673c770caf5f793
David Dukoski <duko_7@outlook.com>
2025-09-03 21:22:25 +0200

    feat: Added payment processing with DUMMY card info

---
+++ src/main/java/mk/route/routemk/exceptions/PaymentProcessException.java
@@ -0,0 +1,7 @@
+package mk.route.routemk.exceptions;
+
+public class PaymentProcessException extends IllegalArgumentException {
+    public PaymentProcessException(String message) {
+        super(message);
+    }
+}
--- src/main/java/mk/route/routemk/services/interfaces/PaymentService.java
+++ src/main/java/mk/route/routemk/services/interfaces/PaymentService.java
@@ -1,7 +1,11 @@
 package mk.route.routemk.services.interfaces;
 
+import mk.route.routemk.models.Account;
 import mk.route.routemk.models.Payment;
+import mk.route.routemk.models.Ticket;
+
+import java.util.Collection;
 
 public interface PaymentService extends GenericService<Payment, Integer> {
-    //TODO: implement payment processing
+    Collection<? extends Ticket> purchaseTickets(Account account, Integer tripPurchasingForId, Integer numTickets);
 }
